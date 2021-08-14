a45e3270920eb124bc29e52914f6b8e70bb91af6
nullCommit <allangeek@gmail.com>
2021-08-14 11:23:48 -0300

    Challenge completed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,18 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string;
+  icon?: React.ComponentType;
+}
+
+type InputProps = JSX.IntrinsicElements['input'] & Props;
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -37,7 +39,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {Icon && <Icon />}
 
       <input
         onFocus={handleInputFocus}
@@ -48,6 +50,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -1,5 +1,10 @@
 import styled, { css } from 'styled-components';
 
+interface StyleProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
@@ -21,15 +26,15 @@ export const Container = styled.div`
     line-height: 36px;
   }
 
-  ${props =>
-    props.isFocused &&
+  ${({ isFocused }: StyleProps) =>
+    isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
     `}
 
-  ${props =>
-    props.isFilled &&
+  ${({ isFilled }: StyleProps) =>
+    isFilled &&
     css`
       color: #ff9000;
     `}
